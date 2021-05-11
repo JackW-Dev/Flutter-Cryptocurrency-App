e229850c56589d99527b9e0ec0ade303f5f58818
Jack <jd.walker@sky.com>
2021-05-11 15:23:55 +0100

    Update 11/05/2021 Restructured home page and main app page to be one. This is due to wanting passable data without using navigation routes. Implemented a working favourites page. TODO: Make favourites icon persistent after refresh.

--- lib/favouritesPage.dart
+++ lib/favouritesPage.dart
@@ -1,8 +1,14 @@
-import 'package:flutter/material.dart';
 import 'dart:core';
+
+import 'package:flutter/material.dart';
 
 //  This class defines FavouritesPage as being stateful
 class FavouritesPage extends StatefulWidget {
+  final List favourites;
+
+  //  Custom constructor to accept favourites list and use as a param in this class
+  const FavouritesPage({Key key, this.favourites}) : super(key: key);
+
   //  This override will use the state produced by FavouritesPageState()
   @override
   FavouritesPageState createState() => FavouritesPageState();
@@ -12,6 +18,56 @@ class FavouritesPage extends StatefulWidget {
 class FavouritesPageState extends State<FavouritesPage> {
   @override
   Widget build(BuildContext context) {
-    return Text('Favourites');
+    return _favouritesListBuilder();
+  }
+
+  //  Function will build the ListView that displays the crypto values
+  Widget _favouritesListBuilder() {
+    if (widget.favourites.isEmpty) {
+      return Text("Favourites list is empty.");
+    } else {
+      //  Return a ListView using a separated list builder to create items within the ListView
+      return ListView.separated(
+          itemCount: widget.favourites.length,
+          //  Set padding for each side of the list
+          padding: const EdgeInsets.all(16.0),
+          //  Add dividers between each item
+          separatorBuilder: (context, index) {
+            return Divider(color: Colors.black);
+          },
+          //  Define how each item in the ListView is created
+          itemBuilder: (context, i) {
+            final index = i;
+            //  Call the _rowBuilder() function to produce a row using the data passed to it
+            return _rowBuilder(widget.favourites[index]);
+          });
+    }
+  }
+
+  //  Function will build each row within the ListView
+  Widget _rowBuilder(Map crypto) {
+    //  Parse the value of "priceUsd" from the crypto JSON to a double value
+    double price = double.parse(crypto['priceUsd']);
+    //  Round to 6dp and cast into a string
+    String priceStr = "\$" + (price).toStringAsFixed(6);
+
+    //  Return a ListTile item (this will be each item in the list)
+    return ListTile(
+      //  Start with a CircleAvatar widget
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue[200],
+        child: Text(
+          //  This will use the 1st letter of the name
+          crypto['name'][0],
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      title: Text('${crypto['name']} (${crypto['symbol']})'),
+      //  Set the subtitle for the ListTile
+      subtitle: Text(
+        priceStr,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'favouritesPage.dart';
-import 'homePage.dart';
 
-//  This line will call MyApp() as the application to run
-void main() => runApp(MyApp());
+//  This line will call MainPage() as the application to run
+void main() => runApp(MainPage());
 
-//  Class defining MyApp
-class MyApp extends StatelessWidget {
-  //  This override will build a MaterialApp widget and return it using custom data
-  @override
-  Widget build(BuildContext context) {
-    //  Returns a MaterialApp widget
-    return MaterialApp(
-      title: 'Crypto Spy',
-      //  Declare a theme to be used by the app
-      theme: new ThemeData(primaryColor: Colors.amber[400]),
-      //  Define the home page that the app will launch to
-      home: MainPage(),
-    );
-  }
-}
-
-//  This class defines HomePage as being stateful
+//  This class defines MainPage as being stateful
 class MainPage extends StatefulWidget {
   //  This override will use the state produced by HomePageState()
   @override
@@ -33,42 +21,106 @@ class MainPage extends StatefulWidget {
 class MainPageState extends State<MainPage> {
   int _selectedPageIndex = 0;
 
-  //  List of widgets that will be swapped to form the body for each page
-  static List<Widget> _pageContent = <Widget>[
-    HomePage(),
-    FavouritesPage(),
-  ];
+  List _fetchedCrypto;
+
+  //  Declare a set to store favourited currencies
+  final List _favourites = [];
+
+  //  Boolean flag to show if data is loaded or not
+  bool isLoaded;
+
+  //  Override the initState to add additional actions on page load
+  @override
+  void initState() {
+    //  Carry out the standard init process
+    super.initState();
+    //  Call the fetchCrypto() function on load
+    fetchCrypto();
+  }
 
   //  Override the build function to return the layout defined below
   @override
   Widget build(BuildContext context) {
-    //  Return a Scaffold widget that defines the layout of HomePage
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Crypto Spy'),
-      ),
-      //  Set the page content to be chosen using the bottomNavigationBar
-      body: Center(
-        child: _pageContent[_selectedPageIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.amber[400],
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black45,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favourites',
-          ),
-        ],
-        currentIndex: _selectedPageIndex,
-        onTap: _onNavBarItemTapped,
+    //  This override will build a MaterialApp widget and return it using custom data
+    return MaterialApp(
+      title: 'Crypto Spy',
+      //  Declare a theme to be used by the app
+      theme: new ThemeData(primaryColor: Colors.amber[400]),
+      //  Define the home page that the app will launch to
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Crypto Spy'),
+        ),
+        //  Set the page content to be chosen using the bottomNavigationBar
+        body: Center(
+          child: _pageContent(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.amber[400],
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black45,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourites',
+            ),
+          ],
+          currentIndex: _selectedPageIndex,
+          onTap: _onNavBarItemTapped,
+        ),
       ),
     );
+  }
+
+  //  Declare a function to call the API and store the response as JSON objects
+  //  Using async allows this to be carried out as other actions occur
+  //  Future means that a correct response or error can return once the function finishes
+  Future<void> fetchCrypto() async {
+    setState(() {
+      isLoaded = false;
+    });
+    //  API URL that returns data on different cryptos
+    String _apiUrl = 'https://api.coincap.io/v2/assets';
+    //  Make a fetch request to the API using the URL
+    http.Response response = await http.get(Uri.parse(_apiUrl));
+    //  Update the state of the List when a response is received
+    setState(() {
+      //  Decode the JSON object returned, using the JSON array that falls
+      //  under the "data" heading
+      _fetchedCrypto = jsonDecode(response.body)['data'];
+      isLoaded = true;
+    });
+  }
+
+  //  Function to handle which page is being shown as the body
+  //  Case default will show the main crypto list page as a refreshable
+  //  Case 1 will swap the body to show the favourites page
+  _pageContent() {
+    switch (_selectedPageIndex) {
+      case 1:
+        return FavouritesPage(
+          favourites: _favourites,
+        );
+        break;
+      default:
+        //  If data has been loaded
+        if (isLoaded) {
+          //  This will allow pull down to refresh which will update the page content
+          return RefreshIndicator(
+            child: _cryptoListBuilder(),
+            onRefresh: fetchCrypto,
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        break;
+    }
   }
 
   //  Function to handle tap event on navBar
@@ -79,6 +131,78 @@ class MainPageState extends State<MainPage> {
     });
   }
 
-  //  Define the action that will occur when the list icon is tapped
-  void _viewFavourites() {}
+  //  Home page list creation
+
+  //  Function will build the ListView that displays the crypto values
+  Widget _cryptoListBuilder() {
+    //  Return a ListView using a separated list builder to create items within the ListView
+    return ListView.separated(
+        itemCount: _fetchedCrypto.length,
+        //  Set padding for each side of the list
+        padding: const EdgeInsets.all(16.0),
+        //  Add dividers between each item
+        separatorBuilder: (context, index) {
+          return Divider(color: Colors.black);
+        },
+        //  Define how each item in the ListView is created
+        itemBuilder: (context, i) {
+          final index = i;
+          //  Call the _rowBuilder() function to produce a row using the data passed to it
+          return _rowBuilder(_fetchedCrypto[index]);
+        });
+  }
+
+  //  Function will build each row within the ListView
+  Widget _rowBuilder(Map crypto) {
+    //  Check if the favourites list contains the passed in crypto and
+    //  store the result in a boolean variable
+    final bool isFavourite = _favourites.contains(crypto);
+
+    //  This function will be used to add and remove items from the favourited list
+    //  This is called when the heart icon is tapped
+    void _setFavourite() {
+      //  Update the state of the icon when tapped
+      setState(() {
+        if (isFavourite) {
+          _favourites.remove(crypto);
+        } else {
+          _favourites.add(crypto);
+        }
+      });
+    }
+
+    //  Parse the value of "priceUsd" from the crypto JSON to a double value
+    double price = double.parse(crypto['priceUsd']);
+    //  Round to 6dp and cast into a string
+    String priceStr = "\$" + (price).toStringAsFixed(6);
+
+    //  Return a ListTile item (this will be each item in the list)
+    return ListTile(
+      //  Start with a CircleAvatar widget
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue[200],
+        child: Text(
+          //  This will use the 1st letter of the name
+          crypto['name'][0],
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      title: Text('${crypto['name']} (${crypto['symbol']})'),
+      //  Set the subtitle for the ListTile
+      subtitle: Text(
+        priceStr,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      //  Define what will be at the trailing edge of the List Item (in this case an IconButton)
+      trailing: IconButton(
+        //  Icon is set by a conditional statement based on if the item is in
+        //  the favourites list or not
+        icon: Icon(isFavourite ? Icons.favorite : Icons.favorite_border),
+        //  Set the colour of the icon based on the same conditional (null if not fave)
+        color: isFavourite ? Colors.redAccent : null,
+        //  Define the action to be carried out if pressed (_setFavourite())
+        onPressed: _setFavourite,
+      ),
+    );
+  }
 }
